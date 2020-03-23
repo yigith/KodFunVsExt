9160c620b7ab60a44521679ceca729ddebc58a76
Yiğit Hacıefendioğlu <yigith1@gmail.com>
2020-03-23 11:09:09 +0300

    Add project files.

---
+++ KodFunVsExt/GetLastPage.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Newtonsoft.Json;
+using Task = System.Threading.Tasks.Task;
+
+namespace KodFunVsExt
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class GetLastPage
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("d85a8ce2-a980-4549-bb59-ff6c3e05a8ac");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GetLastPage"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private GetLastPage(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static GetLastPage Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in GetLastPage's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new GetLastPage(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            //string message = "merhaba dünya";
+            //string title = "GetLastPage";
+
+            //// Show a message box to prove we were here
+            //VsShellUtilities.ShowMessageBox(
+            //    this.package,
+            //    message,
+            //    title,
+            //    OLEMSGICON.OLEMSGICON_INFO,
+            //    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+            var saveFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments ) + @"\NotKodFun";
+            if (!Directory.Exists(saveFolder))
+            {
+                Directory.CreateDirectory(saveFolder);
+            }
+
+
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            string content = client.DownloadString("https://not.kod.fun/Notlar/Getir");
+            List<Sayfa> sayfalar = JsonConvert.DeserializeObject<List<Sayfa>>(content);
+            
+            if (sayfalar.Count > 0)
+            {
+                var sonSayfa = sayfalar[sayfalar.Count - 1];
+                var savePath = Path.Combine(saveFolder, MakeValidFileName(sonSayfa.baslik) + ".txt");
+                File.WriteAllText(savePath, sonSayfa.icerik);
+                VsShellUtilities.OpenDocument(this.package, savePath);
+            }
+
+        }
+
+        static char[] _invalids;
+        // https://stackoverflow.com/questions/620605/how-to-make-a-valid-windows-filename-from-an-arbitrary-string
+        /// <summary>Replaces characters in <c>text</c> that are not allowed in 
+        /// file names with the specified replacement character.</summary>
+        /// <param name="text">Text to make into a valid filename. The same string is returned if it is valid already.</param>
+        /// <param name="replacement">Replacement character, or null to simply remove bad characters.</param>
+        /// <param name="fancy">Whether to replace quotes and slashes with the non-ASCII characters ” and ⁄.</param>
+        /// <returns>A string that can be used as a filename. If the output string would otherwise be empty, returns "_".</returns>
+        public static string MakeValidFileName(string text, char? replacement = '_', bool fancy = true)
+        {
+            StringBuilder sb = new StringBuilder(text.Length);
+            var invalids = _invalids ?? (_invalids = Path.GetInvalidFileNameChars());
+            bool changed = false;
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (invalids.Contains(c))
+                {
+                    changed = true;
+                    var repl = replacement ?? '\0';
+                    if (fancy)
+                    {
+                        if (c == '"') repl = '”'; // U+201D right double quotation mark
+                        else if (c == '\'') repl = '’'; // U+2019 right single quotation mark
+                        else if (c == '/') repl = '⁄'; // U+2044 fraction slash
+                    }
+                    if (repl != '\0')
+                        sb.Append(repl);
+                }
+                else
+                    sb.Append(c);
+            }
+            if (sb.Length == 0)
+                return "_";
+            return changed ? sb.ToString() : text;
+        }
+    }
+}
---
+++ KodFunVsExt/Sayfa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodFunVsExt
+{
+    public class Sayfa
+    {
+        public string baslik { get; set; }
+        public string icerik { get; set; }
+    }
+}
